52e988b64ba13fbf99790ce9a46ab1b7565b59c6
Luizzmy <luizzmy@example.com>
2020-10-21 22:04:02 -0500

    writeCorrectLetter

--- js/index.js
+++ js/index.js
@@ -1,21 +1,39 @@
-// ITERATION 1
+
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = parseInt(product.querySelector('.price span').innerHTML);
+  // const priceValue = price.getAttribute('span');
+  const quantity = parseInt(product.querySelector('.quantity input').value);
+  // let subtotal = document.getElementsByClassName('subtotal');
+  let subtotal = price*quantity;
+  // subtotal.innerHTML = `${(price)*(quantity)}`;
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+  return subtotal;
+  // console.log(quantity)
+  // // console.log (priceValue)
+  // console.log (subtotal)
+  // console.log(price)
 }
-
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+   const products = document.getElementsByClassName('product');
+   let total = 0;
+    for (let i = 0; i < products.length; i++) {
+     singleprod=products[i]
+      updateSubtotal(singleprod)
+      total += parseFloat(singleprod.querySelector('.subtotal span').innerHTML);
+    }
+  document.querySelector('#total-value span').innerHTML=`${total}`
+  //  const singleProduct = document.querySelector('.product');
+  // console.log(singleProduct)
+  
+  //  updateSubtotal(singleProduct);
 
+
+   //updateSubtotal(products);
+  // end of test
   // ITERATION 2
   //... your code goes here
-
   // ITERATION 3
   //... your code goes here
 }
